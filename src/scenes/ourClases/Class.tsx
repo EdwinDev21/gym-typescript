5d1481a7ae4ac25790ea923a088ddb3f22e70044
Edwin ayerbe <edwinayerbe38@gmail.com>
2024-11-08 14:52:28 +0000

    fix-index-OurClasses

--- src/scenes/ourClases/Class.tsx
+++ src/scenes/ourClases/Class.tsx
@@ -8,7 +8,7 @@ type Props = {
 }
 
 const Class = ({image,name,description}: Props) => {
-  const overlayStyle = `p-5 absolute z-30 flex
+  const overlayStyle = `p-5 absolute z-10 flex
   h-[380px] w-[450px] flex-col items-center justify-center
   whitespace-normal bg-primary-500 text-center text-white
   opacity-0 transition duration-500 hover:opacity-90
